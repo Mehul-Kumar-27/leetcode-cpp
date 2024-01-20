a9f8087fd9f2e0109efdd9b547e6598d2fb1cf5e
Mehul-Kumar-27 <mehul2002kumar@gmail.com>
2024-01-20 16:26:17 +0530

    set matrix to zero

---
+++ 73.set-matrix-zeroes.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=73 lang=golang
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+func setZeroes(matrix [][]int)  {
+    var row int = len(matrix)
+	var col int = len(matrix[0])
+
+	var r = make([]int, row)
+	var c = make([]int, col)
+
+	for i := 0 ; i < row; i++{
+		for j := 0; j < col; j++{
+			if matrix[i][j] == 0{
+				r[i] = 1
+				c[j] = 1
+			}
+		}
+	}
+
+	for i := 0 ; i < row; i++{
+		for j := 0; j < col; j++{
+			if r[i] == 1 || c[j] == 1{
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+}
+// @lc code=end
+
